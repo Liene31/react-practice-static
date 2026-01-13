4f5b8c209518c06c401ee159204334b45d7dca81
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-01-13 22:23:56 +0100

    create simple page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <main>
+    <img src="react-logo.png" width="40px" alt="react-logo" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars in GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </main>
+);
